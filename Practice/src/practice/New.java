4e1cbbe386a5f9732afa0204c87312f1e83f433f
TejuDhobaleQA <tejudhobale718@gmail.com>
2023-01-01 19:48:20 +0530

    commit

--- Practice/src/practice/New.java
+++ Practice/src/practice/New.java
@@ -1,11 +1,24 @@
 package practice;
 
-
-
 public class New {
 	
 public static void main(String[] args) {
 	
+	
+	int a=153; //1+125+27=153
+	int sum=0;   //
+	
+	for(int i=1;i<=a;) {  //1<=153   1<=15 1<=1
+		for(int j=1;j<=a;j++) {// 1<=1   1<=1  1<=1
+			int c=a%10;  //3  5  1
+			
+			int m = (int) Math.pow(c, 3); //3^3  5^3  1^1
+			sum=sum+m; //27  27+125=152  152+1=153
+			a=a/10;   //15   1  
+		}
+		
+	}
+	System.out.println(sum);
 	
 }
 
